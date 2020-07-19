81a7862aac840d94ae9267da7441578149c11e43
Siri Bafna <bafnasiri@gmail.com>
2020-07-18 21:44:23 -0500

    updated purchase again

--- hibernateProjectForStudent/src/cs4347/hibernateProject/ecomm/services/impl/PurchasePersistenceServiceImpl.java
+++ hibernateProjectForStudent/src/cs4347/hibernateProject/ecomm/services/impl/PurchasePersistenceServiceImpl.java
@@ -7,11 +7,12 @@
  * failure of the team responsible and any team that uses the shared materials. 
  * Sharing project materials or using shared materials will also result 
  * in the reporting of all team members for academic dishonesty. 
- */ 
- 
+ */
+
 package cs4347.hibernateProject.ecomm.services.impl;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -22,51 +23,115 @@ import cs4347.hibernateProject.ecomm.services.PurchasePersistenceService;
 import cs4347.hibernateProject.ecomm.services.PurchaseSummary;
 import cs4347.hibernateProject.ecomm.util.DAOException;
 
-public class PurchasePersistenceServiceImpl implements PurchasePersistenceService
-{
-	@PersistenceContext 
-	private EntityManager em; 
-	
+public class PurchasePersistenceServiceImpl implements PurchasePersistenceService {
+	@PersistenceContext
+	private EntityManager em;
+
 	public PurchasePersistenceServiceImpl(EntityManager em) {
 		this.em = em;
 	}
-	
+
 	@Override
-	public void create(Purchase purchase) throws SQLException, DAOException
-	{
+	public void create(Purchase purchase) throws SQLException, DAOException {
+		try {
+			em.getTransaction().begin();
+			em.persist(purchase);
+			em.getTransaction().commit();
+		} catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
-	public Purchase retrieve(Long id) throws SQLException, DAOException
-	{
-		return null;
+	public Purchase retrieve(Long id) throws SQLException, DAOException {
+		try {
+			em.getTransaction().begin();
+			Purchase purchase = em.find(Purchase.class, id);
+			em.getTransaction().commit();
+			return purchase;
+		} catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
-	public void update(Purchase purchase) throws SQLException, DAOException
-	{
+	public void update(Purchase purchase) throws SQLException, DAOException {
+		try {
+			em.getTransaction().begin();
+			Purchase p2 = em.find(Purchase.class, purchase.getId());
+			p2.setPurchaseDate(purchase.getPurchaseDate());
+			p2.setPurchaseAmount(purchase.getPurchaseAmount());
+			p2.setCustomer(purchase.getCustomer());
+			p2.setProduct(purchase.getProduct());
+			em.getTransaction().commit();
+		} catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
-	public void delete(Long id) throws SQLException, DAOException
-	{
+	public void delete(Long id) throws SQLException, DAOException {
+
+		em.getTransaction().begin();
+		Purchase purchase = (Purchase) em.createQuery("from Purchase as p where p.id = :id").setParameter("id", id)
+				.getSingleResult();
+
+		if (purchase == null) {
+			em.getTransaction().rollback();
+			throw new DAOException("Purchase ID Not Found " + id);
+		}
+
+		em.remove(purchase); // Does this work?
+		em.getTransaction().commit();
 	}
 
 	@Override
-	public List<Purchase> retrieveForCustomerID(Long customerID) throws SQLException, DAOException
-	{
-		return null;
+	public List<Purchase> retrieveForCustomerID(Long customerID) throws SQLException, DAOException {
+		em.getTransaction().begin();
+		List<Purchase> purchases = (List<Purchase>) em.createQuery("from Purchase as p  where p.customer.id = :custId")
+				.setParameter("custId", customerID).getResultList();
+
+		em.getTransaction().commit();
+		return purchases;
 	}
 
 	@Override
-	public PurchaseSummary retrievePurchaseSummary(Long customerID) throws SQLException, DAOException
-	{
-		return null;
+	public PurchaseSummary retrievePurchaseSummary(Long customerID) throws SQLException, DAOException {
+		List<Purchase> purchases = new ArrayList<Purchase>();
+		purchases = retrieveForCustomerID(customerID);
+		PurchaseSummary result = new PurchaseSummary();
+		double minimum = 999999999999.99;
+		double maximum = 0;
+		double sum = 0;
+		for (Purchase p : purchases) {
+			double temp = p.getPurchaseAmount();
+			if (temp < minimum) {
+				minimum = temp;
+			}
+			if (temp > maximum) {
+				maximum = temp;
+			}
+			sum += temp;
+		}
+		double avg = (sum / purchases.size());
+
+		result.minPurchase = (float) minimum;
+		result.maxPurchase = (float) maximum;
+		result.avgPurchase = (float) avg;
+
+		return result;
 	}
 
 	@Override
-	public List<Purchase> retrieveForProductID(Long productID) throws SQLException, DAOException
-	{
-		return null;
+	public List<Purchase> retrieveForProductID(Long productID) throws SQLException, DAOException {
+		em.getTransaction().begin();
+		List<Purchase> purchases = (List<Purchase>) em.createQuery("from Purchase as p  where p.product.id = :prodId")
+				.setParameter("prodId", productID).getResultList();
+
+		em.getTransaction().commit();
+		return purchases;
 	}
 }
